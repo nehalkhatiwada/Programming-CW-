9dc395bc89279a109bcc4cf8229df8caf82c28f2
nehalkhatiwada <112367599+nehalkhatiwada@users.noreply.github.com>
2024-02-07 14:02:17 +0545

    encrypt

--- CW1.py
+++ CW1.py
@@ -10,3 +10,29 @@ c = {}
 for i in range(255):
     d[chr(i)] = i
     c[i] = chr(i)
+def encrypt_image():
+    filename = filedialog.askopenfilename(title="Select Image")
+    if filename:
+        x = cv2.imread(filename)
+
+        key = key_entry.get()
+        text = text_entry.get()
+
+        kl = 0
+        tln = len(text)
+        z = 0  # decides plane
+        n = 0  # number of row
+        m = 0  # number of column
+
+        l = len(text)
+
+        for i in range(l):
+            x[n, m, z] = d[text[i]] ^ d[key[kl]]
+            n = n + 1
+            m = m + 1
+            m = (m + 1) % 3
+            kl = (kl + 1) % len(key)
+
+        cv2.imwrite("encrypted_img.jpg", x)
+        os.startfile("encrypted_img.jpg")
+        result_label.config(text="Data Hiding in Image completed successfully.")
